cbf4bf3b6bc8acd0a8bd7a3835cb39e5f02dcd85
Oscart <102703028@nccu.edu.tw>
2016-03-25 08:30:18 +0800

    add hamburger bar

--- Android-NiceTab-master/demo/src/main/java/me/amiee/nicetab/demo/MainActivity.java
+++ Android-NiceTab-master/demo/src/main/java/me/amiee/nicetab/demo/MainActivity.java
@@ -3,11 +3,15 @@ package me.amiee.nicetab.demo;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 import me.amiee.nicetab.NiceTabLayout;
@@ -15,6 +19,7 @@ import me.amiee.nicetab.NiceTabStrip;
 
 public class MainActivity extends AppCompatActivity implements NiceTabStrip.OnIndicatorColorChangedListener {
     private Toolbar mToolbar;
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +32,40 @@ public class MainActivity extends AppCompatActivity implements NiceTabStrip.OnIn
         setSupportActionBar(mToolbar);
 
         mToolbar.showOverflowMenu();
+
+
+        //set for navigation drawer menu click responed
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        NavigationView view = (NavigationView) findViewById(R.id.navigation_view);
+        view.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                Toast.makeText(MainActivity.this, menuItem.getTitle() + " pressed", Toast.LENGTH_LONG).show();
+                ;
+
+                menuItem.setChecked(true);
+                drawerLayout.closeDrawers();
+                return true;
+            }
+        });
+        //drawer
+
+        //hamburger bar icon
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle( this, drawerLayout, mToolbar, R.string.open , R.string.close){
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super .onDrawerClosed(drawerView);
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super .onDrawerOpened(drawerView);
+            }
+        };
+
+        drawerLayout.setDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+        //hamburger bar icon
 
         if (savedInstanceState == null) {
             FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
