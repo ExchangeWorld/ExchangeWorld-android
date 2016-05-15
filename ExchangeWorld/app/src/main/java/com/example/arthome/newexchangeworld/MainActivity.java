59f4bde0ba245f2873eff2e55a2ff7b683c1ac09
Oscart <102703028@nccu.edu.tw>
2016-05-15 22:18:55 +0800

    remove setting menu(toolbar)

--- ExchangeWorld/app/src/main/java/com/example/arthome/newexchangeworld/MainActivity.java
+++ ExchangeWorld/app/src/main/java/com/example/arthome/newexchangeworld/MainActivity.java
@@ -76,7 +76,7 @@ public class MainActivity extends AppCompatActivity
             super.onBackPressed();
         }
     }
-
+/*              three dot setting on toolbar
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -98,7 +98,7 @@ public class MainActivity extends AppCompatActivity
         }
 
         return super.onOptionsItemSelected(item);
-    }
+    }*/
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
