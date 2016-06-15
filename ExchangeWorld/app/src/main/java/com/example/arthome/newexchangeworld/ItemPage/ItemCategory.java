632eded0b507d4fd3792c6afea496e0d114ba324
oscart820102 <102703028@nccu.edu.tw>
2016-06-15 18:06:33 +0800

    category click and downlad, not finished yet

--- ExchangeWorld/app/src/main/java/com/example/arthome/newexchangeworld/ItemPage/ItemCategory.java
+++ ExchangeWorld/app/src/main/java/com/example/arthome/newexchangeworld/ItemPage/ItemCategory.java
@@ -35,14 +35,23 @@ public class ItemCategory extends Fragment implements View.OnClickListener {
     @Override
     public void onClick(View v) {
         //Toast.makeText(getContext(),Integer.toString(v.getId())+" pressed",Toast.LENGTH_SHORT).show();
+        FragmentTransaction transaction;
+        Bundle bundle;
+        Fragment fragment;
         switch (v.getId()){
             case R.id.category_3c:
                 //TODO find fragment then transition
                 //Fragment page = getParentFragment().getChildFragmentManager().findFragmentByTag("android:switcher:" + R.id.Viewpager + ":1");
                 //if(page!=null){
                     //Toast.makeText(getContext(),"good",Toast.LENGTH_SHORT).show();
-                    FragmentTransaction transaction = getFragmentManager().beginTransaction();
-                    transaction.replace(R.id.fragment_category, ItemFragment.newInstance()).addToBackStack(null);
+                    //pass the download_url to fragment
+                    bundle = new Bundle();
+                    bundle.putString("URL","http://exwd.csie.org:43002/api/goods/search");
+                    fragment = ItemFragment.newInstance();
+                    fragment.setArguments(bundle);
+
+                    transaction = getFragmentManager().beginTransaction();
+                    transaction.replace(R.id.fragment_category, fragment).addToBackStack(null);
                     transaction.commit();
                 //}
                 //else {
@@ -51,14 +60,38 @@ public class ItemCategory extends Fragment implements View.OnClickListener {
 
                 break;
             case R.id.category_others:
+                bundle = new Bundle();
+                bundle.putString("URL","http://exwd.csie.org:43002/api/goods/search?category=Others");
+                fragment = ItemFragment.newInstance();
+                fragment.setArguments(bundle);
+
+                transaction = getFragmentManager().beginTransaction();
+                transaction.replace(R.id.fragment_category, fragment).addToBackStack(null);
+                transaction.commit();
                 break;
             case R.id.category_cosmetic:
                 break;
             case R.id.category_clothes:
                 break;
             case R.id.category_textbooks:
+                bundle = new Bundle();
+                bundle.putString("URL","http://exwd.csie.org:43002/api/goods/search?category=Textbooks");
+                fragment = ItemFragment.newInstance();
+                fragment.setArguments(bundle);
+
+                transaction = getFragmentManager().beginTransaction();
+                transaction.replace(R.id.fragment_category, fragment).addToBackStack(null);
+                transaction.commit();
                 break;
             case R.id.category_books:
+                bundle = new Bundle();
+                bundle.putString("URL","http://exwd.csie.org:43002/api/goods/search?category=books");
+                fragment = ItemFragment.newInstance();
+                fragment.setArguments(bundle);
+
+                transaction = getFragmentManager().beginTransaction();
+                transaction.replace(R.id.fragment_category, fragment).addToBackStack(null);
+                transaction.commit();
                 break;
         }
     }
