48fc505ba8d98462494475843224edee50b41b81
Oscart <102703028@nccu.edu.tw>
2016-11-03 19:31:37 +0800

    完成CustomCategoryView 加入所有類別

--- app/src/main/java/com/example/arthome/newexchangeworld/CustomViews/CustomCategoryView.java
+++ app/src/main/java/com/example/arthome/newexchangeworld/CustomViews/CustomCategoryView.java
@@ -1,6 +1,8 @@
 package com.example.arthome.newexchangeworld.CustomViews;
 
 import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.widget.ImageView;
@@ -18,25 +20,34 @@ public class CustomCategoryView extends LinearLayout {
 
     private ImageView categoryImageView;
     private TextView categoryTextView;
-
-    public CustomCategoryView(Context context) {
-        super(context);
-        init(context);
-    }
+    private Drawable drawable;
+    private String categoryName;
 
     public CustomCategoryView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        init(context);
+        init(context, attrs);
     }
 
     public CustomCategoryView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        init(context);
+        init(context, attrs);
     }
 
-    private void init(Context context){
+    private void init(Context context, AttributeSet attrs){
         LayoutInflater.from(context).inflate(R.layout.custom_category_view, this, true);
         categoryImageView = (ImageView) findViewById(R.id.custom_category_image_view);
         categoryTextView = (TextView) findViewById(R.id.custom_category_text_view);
+
+
+        TypedArray a = context.getTheme().obtainStyledAttributes(attrs, R.styleable.CustomCategoryView, 0, 0);
+        try {
+            drawable = a.getDrawable(R.styleable.CustomCategoryView_categoryImageID);
+            categoryName = a.getString(R.styleable.CustomCategoryView_categoryName);
+        }
+        finally {
+            a.recycle();
+        }
+        categoryTextView.setText(categoryName);
+        categoryImageView.setImageDrawable(drawable);
     }
 }
