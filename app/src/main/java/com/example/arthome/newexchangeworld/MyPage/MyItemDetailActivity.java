cbc6d25d1cb2b5945ce57600512784b4d9dd96ae
Oscart <102703028@nccu.edu.tw>
2016-11-06 14:50:59 +0800

    MyItemDetail頁面 編輯功能加入類別選項

--- app/src/main/java/com/example/arthome/newexchangeworld/MyPage/MyItemDetailActivity.java
+++ app/src/main/java/com/example/arthome/newexchangeworld/MyPage/MyItemDetailActivity.java
@@ -23,6 +23,8 @@ import com.example.arthome.newexchangeworld.User;
 import com.example.arthome.newexchangeworld.util.StringTool;
 import com.google.gson.Gson;
 
+import java.util.Arrays;
+
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -30,18 +32,25 @@ import retrofit2.Response;
 
 public class MyItemDetailActivity extends AppCompatActivity {
 
-    TextView goodsDescription;
-    TextView goodsName;
-    EditText goodsDescriptionEditText, goodsNameEditText;
-    LinearLayout whoQueueLayout, deleteLayout, editLayout, shareLayout, infoLayout;
-    RecyclerView imageRecyclerView;
-    Button saveEditButton;
-    User user;
+    private TextView goodsDescription;
+    private TextView goodsName;
+    private EditText goodsDescriptionEditText, goodsNameEditText;
+    private LinearLayout whoQueueLayout, deleteLayout, editLayout, shareLayout, infoLayout, categoryLayout;
+    private RecyclerView imageRecyclerView;
+    private Button saveEditButton, categoryEditButton;
+    private User user;
+    private GoodsModel goodsModel;
+    private String[] categoryName;
+    private String[] categoryValue;
+    private int categoryPicked;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_item_detail);
+
+        categoryName = getResources().getStringArray(R.array.classChinese);
+        categoryValue = getResources().getStringArray(R.array.classType);
 
         // Showing and Enabling clicks on the Home/Up button
         if (getSupportActionBar() != null) {
@@ -57,7 +66,7 @@ public class MyItemDetailActivity extends AppCompatActivity {
         Bundle bundle = getIntent().getExtras();
         if (bundle != null) {
             String json = bundle.getString("goodModel");
-            final GoodsModel goodsModel = new Gson().fromJson(json, GoodsModel.class);
+            goodsModel = new Gson().fromJson(json, GoodsModel.class);
 
             imageRecyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, true));
             imageRecyclerView.setAdapter(new ItemDetailAdapter(goodsModel.getPhoto_path(), this));
@@ -75,11 +84,27 @@ public class MyItemDetailActivity extends AppCompatActivity {
                 }
             });
 
+            categoryEditButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    android.app.AlertDialog.Builder classList = new android.app.AlertDialog.Builder(MyItemDetailActivity.this);
+                    classList.setTitle("選擇類別");
+                    classList.setItems(categoryName, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            categoryEditButton.setText(categoryName[which]);
+                            categoryPicked = which;
+                        }
+                    });
+                    classList.show();
+                }
+            });
+
             saveEditButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     editGoodsAPI(goodsModel);
-                    //TODO  call api
                     setNormalView();
                 }
             });
@@ -114,71 +139,79 @@ public class MyItemDetailActivity extends AppCompatActivity {
         deleteCall.enqueue(new Callback<ResponseBody>() {
             @Override
             public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
-                if(response.code()==200){
-                    Toast.makeText(getApplicationContext(),"刪除成功",Toast.LENGTH_SHORT).show();
+                if (response.code() == 200) {
+                    Toast.makeText(getApplicationContext(), "刪除成功", Toast.LENGTH_SHORT).show();
                     finish();
-                }else {
-                    Toast.makeText(getApplicationContext(),"刪除失敗 status code錯誤",Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "刪除失敗 status code錯誤", Toast.LENGTH_SHORT).show();
                 }
             }
 
             @Override
             public void onFailure(Call<ResponseBody> call, Throwable t) {
-                Toast.makeText(getApplicationContext(),"刪除失敗 onFailure",Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "刪除失敗 onFailure", Toast.LENGTH_SHORT).show();
             }
         });
     }
 
-    private void editGoodsAPI(final GoodsModel goodsModel){
-        EditGoodsModel editGoodsModel = new EditGoodsModel(goodsModel.getGid(),goodsModel.getPosition_x(),goodsModel.getPosition_y(),goodsModel.getPhoto_path(),goodsModel.getCategory());
+    private void editGoodsAPI(final GoodsModel goodsModel) {
+        EditGoodsModel editGoodsModel = new EditGoodsModel(goodsModel.getGid(), goodsModel.getPosition_x(), goodsModel.getPosition_y(), goodsModel.getPhoto_path(), goodsModel.getCategory());
         final String editedName = goodsNameEditText.getText().toString();
         final String editedDescription = goodsDescriptionEditText.getText().toString();
 
         editGoodsModel.setName(editedName);
         editGoodsModel.setDescription(editedDescription);
+        editGoodsModel.setCategory(categoryValue[categoryPicked]);
         Call<ResponseBody> editCall = new RestClient().getExchangeService().editGoods(user.getExToken(), editGoodsModel);
         editCall.enqueue(new Callback<ResponseBody>() {
             @Override
             public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
-                if(response.code()==200){
-                    Toast.makeText(getApplicationContext(),"編輯成功",Toast.LENGTH_SHORT).show();
+                if (response.code() == 200) {
+                    Toast.makeText(getApplicationContext(), "編輯成功", Toast.LENGTH_SHORT).show();
                     goodsName.setText(editedName);
                     goodsDescription.setText(editedDescription);
                     goodsModel.setDescription(editedName);
                     goodsModel.setDescription(editedDescription);
-                }else {
-                    Toast.makeText(getApplicationContext(),"編輯失敗 status code錯誤",Toast.LENGTH_SHORT).show();
+                    goodsModel.setCategory(categoryValue[categoryPicked]);
+                } else {
+                    Toast.makeText(getApplicationContext(), "編輯失敗 status code錯誤", Toast.LENGTH_SHORT).show();
                 }
             }
 
             @Override
             public void onFailure(Call<ResponseBody> call, Throwable t) {
-                Toast.makeText(getApplicationContext(),"編輯失敗 onFailure",Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "編輯失敗 onFailure", Toast.LENGTH_SHORT).show();
             }
         });
     }
 
-    private void setNormalView(){
+    private void setNormalView() {
         goodsName.setVisibility(View.VISIBLE);
         goodsDescription.setVisibility(View.VISIBLE);
         infoLayout.setVisibility(View.VISIBLE);
 
         saveEditButton.setVisibility(View.INVISIBLE);
         goodsDescriptionEditText.setVisibility(View.INVISIBLE);
+        categoryLayout.setVisibility(View.INVISIBLE);
         goodsNameEditText.setVisibility(View.INVISIBLE);
     }
 
-    private void setEditView(){
+    private void setEditView() {
         goodsName.setVisibility(View.INVISIBLE);
         goodsDescription.setVisibility(View.INVISIBLE);
         infoLayout.setVisibility(View.INVISIBLE);
 
         saveEditButton.setVisibility(View.VISIBLE);
         goodsDescriptionEditText.setVisibility(View.VISIBLE);
+        categoryLayout.setVisibility(View.VISIBLE);
         goodsNameEditText.setVisibility(View.VISIBLE);
 
         goodsNameEditText.setText(goodsName.getText());
         goodsDescriptionEditText.setText(goodsDescription.getText());
+        int categoryIndex = Arrays.asList(categoryValue).indexOf(goodsModel.getCategory());
+        categoryIndex = (categoryIndex==-1)?0:categoryIndex;    //防止物品沒有category導致回傳-1crash
+        categoryPicked = categoryIndex;
+        categoryEditButton.setText(categoryName[categoryIndex]);
     }
 
     public void findUIViews() {
@@ -188,10 +221,12 @@ public class MyItemDetailActivity extends AppCompatActivity {
         infoLayout = (LinearLayout) findViewById(R.id.item_detail_info_layout);
         shareLayout = (LinearLayout) findViewById(R.id.my_item_detail_share_layout);
         deleteLayout = (LinearLayout) findViewById(R.id.my_item_detail_delete_layout);
+        categoryLayout = (LinearLayout) findViewById(R.id.my_item_detail_category_layout);
         editLayout = (LinearLayout) findViewById(R.id.my_item_detail_edit_layout);
         whoQueueLayout = (LinearLayout) findViewById(R.id.my_item_detail_who_queue_layout);
         goodsDescriptionEditText = (EditText) findViewById(R.id.my_goods_description_detail_edit_text);
         goodsNameEditText = (EditText) findViewById(R.id.my_goods_name_detail_edit_text);
         saveEditButton = (Button) findViewById(R.id.my_item_detail_save_edit_button);
+        categoryEditButton = (Button) findViewById(R.id.my_item_detail_category_button);
     }
 }
